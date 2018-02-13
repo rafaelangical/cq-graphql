6a2f1bc9877711886974a6d414f60b30bfb65ecc
rafaelangical <rsnrafael@outlook.com>
2018-02-13 12:13:57 -0300

    Updates in apollo.js

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -1,3 +1,12 @@
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
-import { inMemoryCache } from 'apollo-cache-inmemory'
+import { inMemoryCache, InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cjdko4wfb0jrm0194c68pal2l'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cahe: new InMemoryCache()
+})
