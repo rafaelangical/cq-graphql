fee2ca19a96a59ccd599887552dae56b3dd9e269
rafaelangical <rsnrafael@outlook.com>
2018-02-14 17:34:12 -0300

    Created todos by api

--- src/componentes/TodoList.js
+++ src/componentes/TodoList.js
@@ -1,13 +1,38 @@
-import React,{ Component } from 'react'
+import React,{ Component, Fragment } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-export default class TodoList extends Component {
+class TodoList extends Component {
+
+  renderTodoList = () => (
+    <ul>
+      { this.props.todos.allTodoes.map(todo => (
+        <li key={todo.id}>{ todo.text}</li>  
+      )) }
+    </ul>
+  );
+
   render(){
+
+    const { todos } = this.props;
+
     return(
-      <ul>
-        <li>estudar</li>
-        <li>fazer almoco</li>
-        <li>Fazer cafe</li>
-      </ul>
-    )
+      <Fragment>
+      { todos.loading 
+        ? <p>Carregando...</p>
+        : this.renderTodoList() }
+        </Fragment>
+      );
   }
 }
+
+const TodosQuery = gql`
+  query {
+    allTodoes{
+      id
+      text
+    }
+  }
+`;
+
+export default graphql(TodosQuery, { name: 'todos' })(TodoList);
