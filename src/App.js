0a6f6883c09493c2bf3b7e04a8d9e516587545e9
rafaelangical <rsnrafael@outlook.com>
2018-02-14 12:19:57 -0300

    Updates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { ApolloProvider } from 'react-apollo'
 import apolloClient from './services/apollo'
-import ApolloClient from 'apollo-client';
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={ApolloClient}>
+      <ApolloProvider client={apolloClient}>
         <div className="App">
           <h1>Hello Word</h1>
         </div>
