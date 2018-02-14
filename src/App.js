ae63153477dc1b42e220d861f69c3486d5ea73ce
rafaelangical <rsnrafael@outlook.com>
2018-02-14 16:33:36 -0300

    Import TodoList

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { ApolloProvider } from 'react-apollo'
 import apolloClient from './services/apollo'
+import TodoList from './componentes/TodoList'
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={apolloClient}>
-        <div className="App">
-          <h1>Hello Word</h1>
-        </div>
+        <TodoList />
       </ApolloProvider>
     );
   }
