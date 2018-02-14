20d55287e9120d9b8f197efdd92b20529bb1ff66
rafaelangical <rsnrafael@outlook.com>
2018-02-14 12:16:29 -0300

    Config apolloProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { ApolloProvider } from 'react-apollo'
+import apolloClient from './services/apollo'
+import ApolloClient from 'apollo-client';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello Word</h1>
-      </div>
+      <ApolloProvider client={ApolloClient}>
+        <div className="App">
+          <h1>Hello Word</h1>
+        </div>
+      </ApolloProvider>
     );
   }
 }
