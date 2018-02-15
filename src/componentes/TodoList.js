1ef79eb44b5c8097e5802c78686293e38bb0f82d
rafaelangical <rsnrafael@outlook.com>
2018-02-15 17:56:10 -0300

    Import compose

--- src/componentes/TodoList.js
+++ src/componentes/TodoList.js
@@ -1,5 +1,5 @@
 import React,{ Component, Fragment } from 'react'
-import { graphql } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
 
 class TodoList extends Component {
